0607e9bbde928bceeba74c5e03ea19af1795b52c
sehar-abdi456 <sehartech4561@gmail.com>
2024-06-10 13:44:00 +0000

    second

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId= 144559
+let accountEmail= "sehar@gmail.com"
+var accountPassword="1234"
+accountCity="samar"
+
+
+// accountId=2
+console.log(accountId);
+accountEmail="samar@gmail.com"
+accountPassword="398429"
+accountCity="shazia"
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
