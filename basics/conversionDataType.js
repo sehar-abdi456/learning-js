e8c88b8996970fb09e70426b363408e462a50699
sehar-abdi456 <sehartech4561@gmail.com>
2024-06-10 14:26:10 +0000

    conversion of data types

---
+++ basics/conversionDataType.js
@@ -0,0 +1,13 @@
+"use strict";
+let score= "3nsk"
+console.log(typeof score);
+console.log(typeof(score));
+
+let value= Number(score);
+console.log(typeof value);
+console.log(value)
+//it prints that the type is number altho the type in reality also includes character 
+//issue with javascript 
+let isloggedin=1;
+let booleanis= Boolean(isloggedin)
+console.log(isloggedin)
