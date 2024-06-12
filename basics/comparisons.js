f5b941ae484ed040f9474e29a80451fac37d0b35
sehar-abdi456 <sehartech4561@gmail.com>
2024-06-12 17:30:26 +0000

    comparisons

---
+++ basics/comparisons.js
@@ -0,0 +1,5 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null<=0)
+console.log(null>=0)
+//the reason that an equality operator and comparison operators work differently, comparison operartor turns null into 0
